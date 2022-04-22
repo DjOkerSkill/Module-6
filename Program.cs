a076049d4769f0006bfaec6bc84333801f9517c3
Роман <ram23@mail.ru>
2022-04-22 20:26:42 +0300

    Task 6.2.8

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace ConsoleApp4
 {
+    public class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen() 
+        {
+            color = "Black";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int pencost) 
+        { 
+            color=penColor;
+            cost = pencost;
+        }
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ConsoleApp4
+{
+    internal class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public Rectangle() 
+        {
+            a = 6;
+            b = 4;
+        }
+
+        public Rectangle(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public Rectangle(int c)
+        {
+            a = b = c;
+        }
+
+        public int Square() 
+        { 
+            return a * b;
+        }
+
+    }
+}
